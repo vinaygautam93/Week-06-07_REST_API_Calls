9c871ead99479c3b01dc9a95aca6d015053a2d15
vinaygautam93 <vinaygautam935680@gmail.com>
2025-02-22 13:25:16 +0530

    [Vinay] Added default test class

---
+++ src/test/java/com/example/hello_rest_api/HelloRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
